879fbfd63f726abe4003b21a9fc4c325b1b30b27
Genix-JS <111651197+Genix-jailbreak@users.noreply.github.com>
2024-09-26 10:08:57 +0100

    Update script.js
    Part five and the final part of the contact form series

--- script.js
+++ script.js
@@ -158,3 +158,142 @@ function validateAllInputs(firstName, lastName, emailAddress, queryTypes, messag
         isConsentCheckboxValid
     ]
 }
+
+function displayAndRemoveRadioSuccessState(queryTypes) {
+    // This function checks for the selected query type radio button and applies the success state to it
+    const queryTypeArray = Array.from(queryTypes)
+
+    queryTypeArray.filter(queryType => {
+        queryType.parentElement.classList.remove('radio-success-state')
+        if (queryType.checked) {
+            queryType.parentElement.classList.add('radio-success-state');
+        }
+    })
+}
+
+function displayErrorOrSuccessStates() {
+    // This function displays the error or success states of the inputs (after the button is clicked) based on the validation states.
+    // It extracts the values from the validateAllInputs
+    // It displays success or error based on the validation state of the inputs.
+
+    // Define input error messages
+    const firstNameErrorMessage = "This field is required";
+    const lastNameErrorMessage = "This field is required";
+    const emailAddressErrorMessage = "Please enter a valid email address";
+    const queryTypeErrorMessage = "Please select a query type";
+    const messageTextAreaErrorMessage = "This field is required";
+    const consentCheckboxErrorMessage = "To submit this form, please consent to being contacted";
+
+    // Extract validation state values
+    const validationStates = validateAllInputs(firstName, lastName, emailAddress, queryTypes, messageTextArea, consentCheckbox);
+
+    // Display success or error based on the validation state of the inputs
+    if (!validationStates[0]) {
+        // display error message under the input and error state around the input
+        firstName.classList.remove('success-state');
+        allErrorMessages[0].textContent = firstNameErrorMessage;
+        firstName.classList.add('error-state');
+    } else {
+        // remove the error message and the error state and add the success state
+        allErrorMessages[0].textContent = "";
+        firstName.classList.remove('error-state');
+        firstName.classList.add('success-state');
+    }
+
+    if (!validationStates[1]) {
+        // display error message under the input and error state around the input
+        lastName.classList.remove('success-state');
+        allErrorMessages[1].textContent = lastNameErrorMessage;
+        lastName.classList.add('error-state');
+    } else {
+        // remove the error message and the error state and add the success state
+        allErrorMessages[1].textContent = "";
+        lastName.classList.remove('error-state');
+        lastName.classList.add('success-state');
+    }
+
+    if (!validationStates[2]) {
+        // display error messages under the input and error state around the input
+        emailAddress.classList.remove('success-state');
+        allErrorMessages[2].textContent = emailAddressErrorMessage;
+        emailAddress.classList.add('error-state');
+    } else {
+        // remove the error message and the error state and add the success state
+        allErrorMessages[2].textContent = "";
+        emailAddress.classList.remove('error-state');
+        emailAddress.classList.add('success-state');
+    }
+    
+    if (!validationStates[3]) {
+        // display error messages under the input and error state around the input
+        allErrorMessages[3].textContent = queryTypeErrorMessage;
+        queryTypes[0].classList.add('error-state');
+        queryTypes[1].classList.add('error-state');
+    } else {
+        // remove the error message and the error state and add the success state
+        allErrorMessages[3].textContent = "";
+        queryTypes[0].classList.remove('error-state');
+        queryTypes[1].classList.remove('error-state');
+        // queryTypes[0].classList.add('success-state');
+        // queryTypes[1].classList.add('success-state');
+
+        displayAndRemoveRadioSuccessState(queryTypes);
+    }
+
+    if (!validationStates[4]) {
+        // display error messages under the input and error state around the input
+        firstName.classList.remove('success-state');
+        allErrorMessages[4].textContent = messageTextAreaErrorMessage;
+        messageTextArea.classList.add('error-state');
+    } else {
+        // remove the error message and the error state and add the success state
+        allErrorMessages[4].textContent = "";
+        messageTextArea.classList.remove('error-state');
+        messageTextArea.classList.add('success-state');
+    }
+
+    if (!validationStates[5]) {
+        // display error messages under the input and error state around the input
+        consentCheckbox.classList.remove('success-state');
+        allErrorMessages[5].textContent = consentCheckboxErrorMessage;
+        consentCheckbox.classList.add('error-state');
+    } else {
+        // remove the error message and the error state and add the success state
+        allErrorMessages[5].textContent = "";
+        consentCheckbox.classList.remove('error-state');
+        consentCheckbox.classList.add('success-state');
+    }
+}
+
+function handleFormSubmit() {
+    // This function handles the contact form submission, by running the validateAllInput function in the display Error / Success state function.
+    displayErrorOrSuccessStates();
+}
+
+// Callback functions are functions that are executed after a specific action has been performed. They are mostly used in asynchronous actions e.g sending data to a server, querying a database, e.t.c
+
+// Submit form event listener
+submitForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // prevent the default form submission behaviour
+    console.log(event.target); // log the event target to the console
+    handleFormSubmit();
+})
+
+// Submit button event listener
+// submitButton.addEventListener('click', handleFormSubmit);
+
+// console.log(firstName);
+
+// console.log(lastName);
+
+// console.log(emailAddress);
+
+// console.log(queryTypes);
+
+// console.log(message);
+
+// console.log(consentCheckbox);
+
+// console.log(submitButton);
+
+// console.log(submitForm)
